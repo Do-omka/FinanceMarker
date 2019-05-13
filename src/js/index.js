17f4b4230a888c6ecb879cb6bf9996a567ee2347
Doomka <do-omer@ya.ru>
2019-05-13 08:28:12 +0400

    slider

--- src/js/index.js
+++ src/js/index.js
@@ -9,5 +9,61 @@
 // }
 
 document.addEventListener('DOMContentLoaded', (e)=> {
-	
+	if (document.querySelector('.slider')) {
+		let slider = document.querySelector('.slider'),
+			slides = slider.querySelector('.slides'),
+			next = slider.querySelector('.controls .next'),
+			prev = slider.querySelector('.controls .prev'),
+			width = slides.querySelector('.slide').clientWidth,
+			total = slides.children.length,
+			showcase = slides.clientWidth / width,
+			current = 1
+			
+		// slides.dataset.active = current
+		
+		if (total > showcase) {
+			next.classList.add('active')
+		}
+		
+		addEventListener('resize', (e)=> {
+			width = slides.querySelector('.slide').clientWidth
+			showcase = slides.clientWidth / width
+			
+			if (showcase > total - current) {
+				current = total - showcase + 1
+			}
+			
+			slides.style.transform = 'translateX(-' + ((current - 1) * width).toString()  + 'px)'
+			
+			if (showcase <= total - current) {
+				next.classList.add('active')
+			} else {
+				next.classList.remove('active')
+			}
+		})
+		
+		next.addEventListener('click', (e)=> {
+			if (next.classList.contains('active')) {
+				current++
+				slides.style.transform = 'translateX(-' + ((current - 1) * width).toString()  + 'px)'
+				
+				prev.classList.add('active')
+				if (showcase > total - current) {
+					next.classList.remove('active')
+				}
+			}
+		})
+		
+		prev.addEventListener('click', function(e) {
+			if (prev.classList.contains('active')) {
+				current--
+				slides.style.transform = 'translateX(-' + ((current - 1) * width).toString()  + 'px)'
+				
+				next.classList.add('active')
+				if (current < 2) {
+					prev.classList.remove('active')
+				}
+			}
+		})
+	}
 })
