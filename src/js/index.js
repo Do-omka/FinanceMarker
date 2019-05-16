7b601947931b44c003c193337e9fe7adc890c5ac
Doomka <do-omer@ya.ru>
2019-05-16 10:04:18 +0400

    swipe for slider

--- src/js/index.js
+++ src/js/index.js
@@ -9,63 +9,110 @@
 // }
 
 document.addEventListener('DOMContentLoaded', (e)=> {
+	
 	if (document.querySelectorAll('.slider')) {
-		let sliders = document.querySelectorAll('.slider');
+		let sliders = document.querySelectorAll('.slider')
 		for (let i = 0; i < sliders.length; i++) {
-			let slides = sliders[i].querySelector('.slides'),
+			let
+				slides = sliders[i].querySelector('.slides'),
 				next = sliders[i].querySelector('.controls .next'),
 				prev = sliders[i].querySelector('.controls .prev'),
 				width = slides.querySelector('.slide').clientWidth,
 				total = slides.children.length,
-				showcase = slides.clientWidth / width,
+				showcase = Math.round(slides.clientWidth / width),
 				current = 1
+			// slides.dataset.active = current
 			
-		// slides.dataset.active = current
-		
-		if (total > showcase) {
-			next.classList.add('active')
-		}
-		
-		addEventListener('resize', (e)=> {
-			width = slides.querySelector('.slide').clientWidth
-			showcase = slides.clientWidth / width
-			
-			if (showcase > total - current) {
-				current = total - showcase + 1
-			}
-			
-			slides.style.transform = 'translateX(-' + ((current - 1) * width).toString()  + 'px)'
-			
+			//activate buttons
 			if (showcase <= total - current) {
 				next.classList.add('active')
-			} else {
-				next.classList.remove('active')
 			}
-		})
-		
-		next.addEventListener('click', (e)=> {
-			if (next.classList.contains('active')) {
-				current++
+			
+			addEventListener('resize', (e)=> {
+				width = slides.querySelector('.slide').clientWidth
+				showcase = Math.round(slides.clientWidth / width)
+				
+				if (showcase > total - current) {
+					current = total - showcase + 1
+				}
+				
 				slides.style.transform = 'translateX(-' + ((current - 1) * width).toString()  + 'px)'
 				
-				prev.classList.add('active')
-				if (showcase > total - current) {
+				if (showcase <= total - current) {
+					next.classList.add('active')
+				} else {
 					next.classList.remove('active')
 				}
-			}
-		})
-		
-		prev.addEventListener('click', function(e) {
-			if (prev.classList.contains('active')) {
-				current--
-				slides.style.transform = 'translateX(-' + ((current - 1) * width).toString()  + 'px)'
 				
-				next.classList.add('active')
 				if (current < 2) {
 					prev.classList.remove('active')
 				}
+			})
+			
+			next.addEventListener('click', (e)=> {
+				if (next.classList.contains('active')) {
+					current++
+					slides.style.transform = 'translateX(-' + ((current - 1) * width).toString()  + 'px)'
+					
+					prev.classList.add('active')
+					if (showcase > total - current) {
+						next.classList.remove('active')
+					}
+				}
+			})
+			
+			prev.addEventListener('click', function(e) {
+				if (prev.classList.contains('active')) {
+					current--
+					slides.style.transform = 'translateX(-' + ((current - 1) * width).toString()  + 'px)'
+					
+					next.classList.add('active')
+					if (current < 2) {
+						prev.classList.remove('active')
+					}
+				}
+			})
+			
+			//slider swipe
+			function handleGalleryMTouchStart(e) {
+				xDown = e.changedTouches[0].clientX
 			}
-		})
+			function handleGalleryMTouchMove(e) {
+				if (!xDown) {
+					return
+				}
+				let xUp = e.changedTouches[0].clientX
+				let xDiff = xDown - xUp
+				if (xDiff > 0)
+				next.dispatchEvent(new Event('click'))
+				else
+				prev.dispatchEvent(new Event('click'))
+				xDown = null
+			}
+			
+			let xDown = null
+			
+			let handleGalleryTouchStart = function(e) {
+				xDown = e.changedTouches[0].clientX
+			}
+			
+			let handleGalleryTouchMove = function(e) {
+				if (!xDown) {
+					return
+				}
+				
+				let xUp = e.changedTouches[0].clientX
+				let xDiff = xDown - xUp
+				if (xDiff > 0)
+				next.dispatchEvent(new Event('click'))
+				else
+				prev.dispatchEvent(new Event('click'))
+				xDown = null
+			}
+			
+			slides.addEventListener('touchstart', handleGalleryMTouchStart)
+			slides.addEventListener('touchmove', handleGalleryMTouchMove)
 		}
 	}
+	
 })
